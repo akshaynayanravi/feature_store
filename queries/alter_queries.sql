18d5f7844e73461a9874847c23d6de506540b45d
Akshay Okali <ak20536578@L-156308400.local>
2024-11-05 00:08:50 +0530

    Add ERR Diagram, Update model names

--- queries/alter_queries.sql
+++ queries/alter_queries.sql
@@ -1,24 +1,24 @@
-ALTER TABLE FeatureValues
-ADD CONSTRAINT fk_feature_id
-FOREIGN KEY (feature_id) REFERENCES Features(feature_id) ON DELETE CASCADE;
+ALTER TABLE feature_values
+ADD CONSTRAINT fk_fv_feature_id
+FOREIGN KEY (feature_id) REFERENCES features(id) ON DELETE CASCADE;
 
-ALTER TABLE ModelFeatures
-ADD CONSTRAINT fk_model_id
-FOREIGN KEY (model_id) REFERENCES Models(model_id) ON DELETE CASCADE;
+ALTER TABLE model_features
+ADD CONSTRAINT fk_mf_model_id
+FOREIGN KEY (model_id) REFERENCES models(id) ON DELETE CASCADE;
 
-ALTER TABLE ModelFeatures
-ADD CONSTRAINT fk_feature_id
-FOREIGN KEY (feature_id) REFERENCES Features(feature_id) ON DELETE CASCADE;
+ALTER TABLE model_features
+ADD CONSTRAINT fk_mf_feature_id
+FOREIGN KEY (feature_id) REFERENCES features(id) ON DELETE CASCADE;
 
-ALTER TABLE FeatureValues
+ALTER TABLE feature_values
 ADD INDEX idx_feature_id (feature_id);  -- Index on feature_id for quick lookups
 
-ALTER TABLE ModelFeatures
+ALTER TABLE model_features
 ADD INDEX idx_model_id (model_id),  -- Index on model_id for quick lookups
 ADD INDEX idx_feature_id (feature_id);  -- Index on feature_id for quick lookups
 
-ALTER TABLE Features
+ALTER TABLE features
 ADD INDEX idx_name (name);  -- Index on the name for quick lookups
 
-ALTER TABLE Models
+ALTER TABLE models
 ADD INDEX idx_name (name);  -- Index on the name for quick lookups
